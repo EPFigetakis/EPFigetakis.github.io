065fe8b62fe9b868073e56bd485c2f502c1688ee
Emanuel Figetakis <emanuelpfigetakis@me.com>
2025-05-08 17:59:23 -0400

    Fixed landing page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
 
-  if (path.includes("index")) {
+  if (path === "/" || path.endsWith("index.html")) {
     document.getElementById("home-link")?.classList.add("active");
   } else if (path.includes("info")) {
     document.getElementById("info-link")?.classList.add("active");
